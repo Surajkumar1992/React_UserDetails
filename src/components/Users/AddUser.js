bd2d285e7f8a598bef7fe1b6238087b210b85b4f
Surajkumar1992 <suraj.subham@gmail.com>
2021-12-25 11:10:25 +0530

    Adding Wrapper & Fragments

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Wrapper from "../Helpers/Wrapper";
 import Button from "../UI/Button";
 
 import Card from "../UI/Card";
 import ErrorModal from "../UI/ErrorModal";
 import classes from './AddUser.module.css';
+import { Fragment } from "react";
 
 const AddUser = props => {
     const [enteredUsername, setEnteredUsername] = useState('');
@@ -45,7 +47,7 @@ const AddUser = props => {
 
 
     return (
-        <div>
+        <Wrapper>
             {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
                 <Card className={classes.input}>
                     <form onSubmit={addUserHandler}>
@@ -56,7 +58,7 @@ const AddUser = props => {
                         <Button type="submit"> Add User </Button>
                     </form>
                 </Card>
-        </div>
+        </Wrapper>
     )};
 
 export default AddUser;
